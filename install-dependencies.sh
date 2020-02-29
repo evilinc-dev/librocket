3136eb5124b76c9613a8fc49072aecc01bdae201
Gabriel Reiser <gabe@gabereiser.com>
2020-02-29 14:07:24 -0700

    Update dependencies to master

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-PWD=`(pwd)`
-LIB_ROCKET_DEPS="$PWD/dependencies"
+BASE=`(pwd)`
+LIB_ROCKET_DEPS="$BASE/dependencies"
 
 if [ ! -d "$LIB_ROCKET_DEPS" ]; then
   echo -e "\x1b[32mChecking out dependencies\x1b[0m"
   git submodule update --recursive --init
 fi
 
-if [ ! -f "$LIB_ROCKET_DEPS/librocket-deps.a" ]; then
-  cd $LIB_ROCKET_DEPS && ./build.sh && cp "$LIB_ROCKET_DEPS/target/install/lib/librocket-deps.a" "$LIB_ROCKET_DEPS/librocket-deps.a"
+if [ ! -f "$LIB_ROCKET_DEPS/lib/librocket-deps.a" ]; then
+  cd $LIB_ROCKET_DEPS && ./build.sh && mv "$LIB_ROCKET_DEPS/target/install" "$BASE/install" && rm -rf "$LIB_ROCKET_DEPS/*" && mv "$BASE/install" "$LIB_ROCKET_DEPS"
 fi
 
 echo -e "\x1b[32;5m///-- Ready to launch --///\x1b[0m"
