2eff50b371ba8c34b96342acbcb142dd9db3685d
Gabriel Reiser <gabe@gabereiser.com>
2020-02-29 14:18:00 -0700

    Fixed up dependency install script

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,13 +3,18 @@
 BASE=`(pwd)`
 LIB_ROCKET_DEPS="$BASE/dependencies"
 
-if [ ! -d "$LIB_ROCKET_DEPS" ]; then
-  echo -e "\x1b[32mChecking out dependencies\x1b[0m"
-  git submodule update --recursive --init
+if [ -d $LIB_ROCKET_DEPS ]; then
+  rm -rf $LIB_ROCKET_DEPS
 fi
 
+git submodule update --recursive --init
+
 if [ ! -f "$LIB_ROCKET_DEPS/lib/librocket-deps.a" ]; then
-  cd $LIB_ROCKET_DEPS && ./build.sh && mv "$LIB_ROCKET_DEPS/target/install" "$BASE/install" && rm -rf "$LIB_ROCKET_DEPS/*" && mv "$BASE/install" "$LIB_ROCKET_DEPS"
+  cd $LIB_ROCKET_DEPS
+  ./build.sh
+  mv $LIB_ROCKET_DEPS/target/install $BASE/install
+  rm -rf $LIB_ROCKET_DEPS
+  mv $BASE/install $LIB_ROCKET_DEPS
 fi
 
 echo -e "\x1b[32;5m///-- Ready to launch --///\x1b[0m"
